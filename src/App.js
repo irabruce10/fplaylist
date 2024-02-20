a6ec3e16ab89ec9f6d9d32000387feeb2bd5bf72
Bruce <irabruce10@gmail.com>
2024-02-20 14:00:18 +0100

    add item

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className="ldl">jdd</p>
+      <h1 className="ld">dfkvdfl'd d;sdld;lfsd</h1>
+      <h1>kdsld</h1>
     </div>
   );
 }
